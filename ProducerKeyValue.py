1acb111b8eaa27e0359a36f4052ab3b3054c088b
karleefidek <kdf131@uregina.ca>
2021-11-02 13:28:14 -0600

    Update files for NoSQL databases

--- ProducerKeyValue.py
+++ ProducerKeyValue.py
@@ -7,16 +7,16 @@ AvroProducerConf = {'bootstrap.servers': 'localhost:9092',
                    }
 
 schema_Path = 'C:/Users/kfide/OneDrive/Documents/GitHub/485Project/schema1.avsc'
-file_path = 'C:/Users/kfide/OneDrive/Documents/GitHub/485Project/2019-02.csv'
+file_path = 'C:/Users/kfide/OneDrive/Documents/GitHub/485Project/2019-01.csv'
 
 value_schema_path = avro.load(schema_Path)
-avroProducer = AvroProducer(AvroProducerConf, default_value_schema=value_schema_path)
-
+key_schema = avro.loads('{"type":"string"}')
+avroProducer = AvroProducer(AvroProducerConf, default_key_schema=key_schema, default_value_schema=value_schema_path)
 
 with open(file_path) as file:
     reader = csv.DictReader(file, delimiter=',')
     
     for row in reader:
-        avroProducer.produce(topic = "trips_sql", value = row)
+        avroProducer.produce(topic = "tripsdata", value = row, key = row['vendorid'] + row['tpep_pickup_datetime'] + row['tpep_dropoff_datetime'])
 
         avroProducer.flush() # STREAM IN REAL TIME
